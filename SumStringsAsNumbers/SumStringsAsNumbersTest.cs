83f0fe2aa8db4e877528d9e3f66f29dc0e0fecdd
Bell Chen Chi Kuo <chikuokuo@msn.com>
2017-10-14 02:52:45 +0800

    refactor

--- SumStringsAsNumbers/SumStringsAsNumbersTest.cs
+++ SumStringsAsNumbers/SumStringsAsNumbersTest.cs
@@ -67,6 +67,8 @@ namespace SumStringsAsNumbers
 
             private Stack<char> augendStack;
 
+            private Stack<string> result = new Stack<string>();
+
             public string Sum(string a, string b) 
             {
                 if (string.IsNullOrEmpty(a) || string.IsNullOrEmpty(a))
@@ -78,29 +80,59 @@ namespace SumStringsAsNumbers
 
                 augendStack = new Stack<char>(b);
 
-                var result = new Stack<string>();
-
                 var carry = 0;
 
                 while (true)
                 {
-                    var addend = addendStack.Count == 0 ? 0 : (int)Char.GetNumericValue(addendStack.Pop());
-                    var augend = augendStack.Count == 0 ? 0 : (int)Char.GetNumericValue(augendStack.Pop());
-                    var sum = addend + augend + carry;
+                    SumNumberAddToStack(ref carry);
 
-                    result.Push((sum % 10).ToString());
-                    carry = sum / 10;
+                    if (!CalCulateFinish()) continue;
 
-                    if (addendStack.Count == 0 && augendStack.Count == 0)
-                    {
-                        if (carry != 0)
-                        {
-                            result.Push(carry.ToString());
-                        }
-                        break;
-                    }
+                    AddLastCarry(carry);
+
+                    break;
                 }
-                return string.Join("", result.ToArray());
+                var resultString = string.Join("", result.ToArray());
+                result.Clear();
+                return resultString;
+            }
+
+            private void SumNumberAddToStack(ref int carry)
+            {
+                var sum = GetSum(carry);
+                result.Push(GetRemainder(sum));
+                carry = sum / 10;
+            }
+
+            private int GetSum(int carry)
+            {
+                var addend = GetNumber(addendStack);
+                var augend = GetNumber(augendStack);
+                var sum = addend + augend + carry;
+                return sum;
+            }
+
+            private bool CalCulateFinish()
+            {
+                return addendStack.Count != 0 || augendStack.Count != 0;
+            }
+
+            private static string GetRemainder(int sum)
+            {
+                return (sum % 10).ToString();
+            }
+
+            private int GetNumber(Stack<char> stack)
+            {
+                return stack.Count == 0 ? 0 : (int)char.GetNumericValue(stack.Pop());
+            }
+
+            private void AddLastCarry(int carry)
+            {
+                if (carry != 0)
+                {
+                    result.Push(carry.ToString());
+                }
             }
         }
     }
